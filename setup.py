8e5d8fe3b0426b726ce0c1bbcd8952d4f3f350bd
S1lentAFK <86916610+S1lentAFK@users.noreply.github.com>
2024-07-02 14:38:26 +0200

    JSONToolKit 0.2.0
    Added changing key JSON values.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='json_toolkit',
-    version='0.1.0',
+    name='json-toolkit',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[],
     entry_points={},
